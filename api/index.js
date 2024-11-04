c17648e8a5e39f0e81bd45f2d84c8a5e9211e11f
Alan <75879852+alanxpo@users.noreply.github.com>
2024-11-03 17:17:00 -0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const app = express();
 app.use(cors());  // Enable CORS for the app
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 class NBAStats {
     static BASE_URL = "https://stats.nba.com/stats/";
 
@@ -166,7 +168,7 @@ app.get('/player/stats', async (req, res) => {
     return res.json(response);
 });
 
-const PORT = 5000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
